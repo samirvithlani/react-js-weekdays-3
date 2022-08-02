3e7e32edb1150df85d882096678af8b822e057d8
samirvithlani <samir.vithlani83955@gmail.com>
2022-08-02 20:47:58 +0530

    Initial Project

--- src/App.js
+++ src/App.js
@@ -2,22 +2,28 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  var age = 25;
+  var name = 'John';
+  var isActive = true;
+  var salary = 12345.56
+
+  var employee = {
+    name:"raj",
+    email:"raj@gmail.com",
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h1>Name={name}</h1>
+      <h2>age = {age}</h2>
+      <h3>salary  ={salary}</h3>
+      <h4>isActive = {isActive?"Active":"Not Active"}</h4>
+
+      <h1>Emp Name = {employee.name}</h1>
+      <h2>Emp Email = {employee.email}</h2>
     </div>
   );
 }
